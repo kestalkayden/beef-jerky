9282ac07721229bf6554d55c33ee2db99189a503
Kestalkayden <kestalkayden@gmail.com>
2020-08-28 00:09:31 -0500

    Initial commit

---
+++ src/main/java/com/kestalkayden/beef/jerky/BeefJerky.java
@@ -0,0 +1,10 @@
+package com.kestalkayden.beef.jerky;
+import net.fabricmc.api.ModInitializer;
+
+public class BeefJerky implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // TODO Auto-generated method stub
+
+    }    
+}
